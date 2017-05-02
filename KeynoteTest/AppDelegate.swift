6fc887819761c73b6408e3e597eaaa56affc6831
txx <xiaoxuantang@txxs-mac-pro-2.local>
2017-05-02 09:28:44 +0800

    Initial Commit

---
+++ KeynoteTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  KeynoteTest
+//
+//  Created by txx on 02/05/2017.
+//  Copyright © 2017 txx. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
